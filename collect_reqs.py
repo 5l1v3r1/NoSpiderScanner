3a57b74049930e1e70617796abaf89183a055e94
liweijian <liwejian.hust@gmail.com>
2013-05-14 17:00:38 +0800

    debug the collect_reqs

--- collect_reqs.py
+++ collect_reqs.py
@@ -11,56 +11,48 @@ from tornado.httpserver import _BadRequestException
 from tornado.options import define, options
 define("port", default=8080)
 
-class Md5Request(object):
-    def __init__(self):
-        self.data = ""
-        self.method = "GET"
-        self.uri = "/"
-        self.version = "HTTP/1.0"
-        self.get_arguments = {}
-        self.post_arguments = {}
-        self.headers = {}
-        self.files = {}
 
-    def parse_request(self, data):
+def parse_request(data):
+    try:
+        data = native_str(data.decode('latin1'))
+        eol = data.find("\r\n")
+        eol2 = data.find("\r\n\r\n")
+        start_line = data[:eol]
+        get_arguments = {}
+        post_arguments = {}
+        files = {}
         try:
-            self.data = data
-            self.data = native_str(data.decode('latin1'))
-            eol = self.data.find("\r\n")
-            eol2 = self.data.find("\r\n\r\n")
-            start_line = self.data[:eol]
-            try:
-                self.method, self.uri, self.version = start_line.split(" ")
-            except ValueError:
-                raise _BadRequestException("Malformed HTTP request line")
-            if not self.version.startswith("HTTP/"):
-                raise _BadRequestException("Malformed HTTP version")
-            try:
-                self.headers = httputil.HTTPHeaders.parse(self.data[eol:eol2])
-            except ValueError:
-                raise _BadRequestException("Malform HTTP headers")
+            method, uri, version = start_line.split(" ")
+        except ValueError:
+            raise _BadRequestException("Malformed HTTP request line")
+        if not version.startswith("HTTP/"):
+            raise _BadRequestException("Malformed HTTP version")
+        try:
+            headers = httputil.HTTPHeaders.parse(data[eol:eol2])
+        except ValueError:
+            raise _BadRequestException("Malform HTTP headers")
 
-            content_length = self.headers.get("Content-Length")
-            content_type = self.headers.get("Content-Type", "")
-            if "?" in uri:
-                qidx = uri.find("?")
-                self.get_arguments = parse_qs_bytes(native_str(uri[qidx+1:]), keep_blank_value=True)
-            if content_length:
-                body = self.data[eol2:].strip()
-                httputil.parse_body_arguments(
-                        content_type, body, self.post_arguments, self.files)
-        except _BadRequestException as e:
-            gen_log.info("Malformed HTTP request:%s", e)
-            return
+        content_length = headers.get("Content-Length")
+        content_type = headers.get("Content-Type", "")
+        path, sep, query = uri.partition('?')
+        get_arguments = parse_qs_bytes(query, keep_blank_values=True)
+        if content_length:
+            body = data[eol2:].strip()
+            httputil.parse_body_arguments(
+                content_type, body, post_arguments, files)
+            return get_arguments,post_arguments,files
+        return get_arguments,{},{}
+    except _BadRequestException as e:
+        gen_log.info("Malformed HTTP request:%s", e)
+        return
 
 
 class Applicaion(tornado.web.Application):
 
     def __init__(self):
-        handlers =
-            [(r"/collect", CollectHandler),
-             (r"/", IndexHandler)]
-        conn = pymongo.Connect("localhost", 27017)
+        handlers = [(r"/collect", CollectHandler),
+                    (r"/", IndexHandler)]
+        conn = pymongo.Connection("localhost", 27017)
         self.db = conn["reqs"]
         tornado.web.Application.__init__(self, handlers, debug=True)
 
@@ -71,6 +63,12 @@ class CollectHandler(tornado.web.RequestHandler):
         user_name = self.get_argument("username", "anonymous")
         internal_ip = self.get_argument("ip", "None")
         req = self.request.arguments.get("request", [""])[0]
+        get_arg, post_arg, files = parse_request(req)
+        args = {}
+        args.update(get_arg)
+        args.update(post_arg)
+        args.update(files)
+        self.write(args)
 
 
 class IndexHandler(tornado.web.RequestHandler):
