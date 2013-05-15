97d27cbfb3fe25510271c424d03ebac547daaa7e
liweijian <liwejian.hust@gmail.com>
2013-05-15 10:51:33 +0800

    write data to mongodb

--- server/collect_reqs.py
+++ server/collect_reqs.py
@@ -74,19 +74,24 @@ class Applicaion(tornado.web.Application):
     def __init__(self):
         handlers = [(r"/collect", CollectHandler),
                     (r"/", IndexHandler)]
-        conn = pymongo.Connection("localhost", 27017)
-        self.db = conn["reqs"]
+        self.conn = pymongo.Connection("localhost", 27017)
+        self.db = self.conn["reqs"]
         tornado.web.Application.__init__(self, handlers, debug=True)
 
 
 class CollectHandler(tornado.web.RequestHandler):
 
+    @property
+    def db(self):
+        return self.application.db
     def post(self):
         user_name = self.get_argument("username", "anonymous")
         internal_ip = self.get_argument("ip", "None")
         req = self.request.arguments.get("request", [""])[0]
         host, path, get_arg, post_arg, files = parse_request(req)
         uniq_code = gen_uniqcode(host, path, get_arg, post_arg, files)
+        #self.db.insert({md5:uniq_code, domain:host,request:req})
+        self.db.requests.insert({"_id":uniq_code, "domain":host,"request":req})
         self.write(uniq_code)
 
 
