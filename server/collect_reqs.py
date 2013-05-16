ec0331d1879254843519f8769f0cd7635e8819d6
liweijian <liweijian.hust@gmail.com>
2013-05-16 16:04:59 +0800

    update the server side to accept the new format request

--- server/collect_reqs.py
+++ server/collect_reqs.py
@@ -7,6 +7,7 @@ import tornado.options
 import tornado.web
 import pymongo
 import hashlib
+import ast
 
 from myconfigparser import get_list
 from tornado.httpserver import _BadRequestException
@@ -16,53 +17,30 @@ define("port", default=8080)
 
 def parse_request(data):
     try:
-        data = native_str(data.decode('latin1'))
-        eol = data.find("\r\n")
-        eol2 = data.find("\r\n\r\n")
-        start_line = data[:eol]
-        get_arguments = {}
-        post_arguments = {}
-        files = {}
-        try:
-            method, uri, version = start_line.split(" ")
-        except ValueError:
-            raise _BadRequestException("Malformed HTTP request line")
-        if not version.startswith("HTTP/"):
-            raise _BadRequestException("Malformed HTTP version")
-        try:
-            headers = httputil.HTTPHeaders.parse(data[eol:eol2])
-        except ValueError:
-            raise _BadRequestException("Malform HTTP headers")
-
-        content_length = headers.get("Content-Length")
-        host = headers.get("Host")
-        content_type = headers.get("Content-Type", "")
-        path, sep, query = uri.partition('?')
+        req = ast.literal_eval(data)
+        path, sep, query = req['uri'].partition('?')
         get_arguments = parse_qs_bytes(query, keep_blank_values=True)
-        if content_length:
-            body = data[eol2:].strip()
-            httputil.parse_body_arguments(
-                content_type, body, post_arguments, files)
-            return host, path, get_arguments, post_arguments, files
-        return host, path, get_arguments, {}, {}
+        post_arguments = parse_qs_bytes(req['body'], keep_blank_values=True)
+        host = req['headers']['Host']
+        headers = req['headers']
+        return headers, host, path, get_arguments, post_arguments
     except _BadRequestException as e:
         gen_log.info("Malformed HTTP request:%s", e)
         return
 
 
-def gen_uniqcode(host, path, get_arg, post_arg, files):
+def gen_uniqcode(path, get_arg, post_arg):
         MVC_PARAMS = get_list('config.ini', 'MVC Params', 'paramList')
         args = {}
         args.update(get_arg)
         args.update(post_arg)
-        args.update(files)
         keys = ''
         for key in sorted(args.iterkeys()):
             if key in MVC_PARAMS:
                 key += '=' + args[key][0]
             keys += key
             keys += '&'
-        uniq_qry = host + path + keys
+        uniq_qry = path + '?' + keys
         m = hashlib.md5()
         m.update(uniq_qry)
         uniq_code = m.hexdigest()
@@ -89,12 +67,12 @@ class CollectHandler(tornado.web.RequestHandler):
         user_name = self.get_argument("username", "anonymous")
         internal_ip = self.get_argument("ip", "None")
         req = self.request.arguments.get("request", [""])[0]
-        host, path, get_arg, post_arg, files = parse_request(req)
-        uniq_code = gen_uniqcode(host, path, get_arg, post_arg, files)
+        headers, host, path, get_arg, post_arg = parse_request(req)
+        uniq_code = gen_uniqcode(path, get_arg, post_arg)
         self.db.requests.insert(
-            {"_id": uniq_code, "host": host, "request": req,
+            {"_id": uniq_code, "host": host, "headers": headers,
                                "path": path, "get_arg": get_arg,
-                               "post_arg": post_arg, "files": files})
+                               "post_arg": post_arg})
         self.write(uniq_code)
 
 
@@ -108,3 +86,4 @@ if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(Applicaion())
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
+
