20e0aa780518d186d06896811c96b5075db3720f
liweijian <liweijian.hust@gmail.com>
2013-05-22 17:48:41 +0800

    change recv timeout to recv end_symbol, and identify the scan mode

--- scanner/agent/agent.py
+++ scanner/agent/agent.py
@@ -12,13 +12,34 @@ parser.read("config.ini")
 AGENTPORT = int(parser.get('Agent', 'port'))
 MONGOIP = parser.get('Master', 'mongo_ip')
 MONGOPORT = int(parser.get('Master', 'mongo_port'))
-DBCONN= pymongo.Connection(MONGOIP, MONGOPORT)
+END_SYMBOL = parser.get('Protocol', 'end_symbol')
+DBCONN = pymongo.Connection(MONGOIP, MONGOPORT)
 REQ_DB = DBCONN.requests
 TASKSINFO = DBCONN.tasksinfo
 
+
+def recv_end(the_socket, End):
+    total_data = []
+    data = ''
+    while True:
+        data = the_socket.recv(8192)
+        if End in data:
+            total_data.append(data[:data.find(End)])
+            break
+        total_data.append(data)
+        if len(total_data) > 1:
+            # check if end_of_data was split
+            last_pair = total_data[-2] + total_data[-1]
+            if End in last_pair:
+                total_data[-2] = last_pair[:last_pair.find(End)]
+                total_data.pop()
+                break
+    return ''.join(total_data)
+
+
 def recv_timeout(conn, timeout=0.5):
     conn.setblocking(0)
-    total_data=[]
+    total_data = []
     data = ''
     begin = time.time()
     while True:
@@ -37,18 +58,26 @@ def recv_timeout(conn, timeout=0.5):
             gen_log.info("recv_timeout failed")
     return ''.join(total_data)
 
+
 def scan_task(data):
+    """
+    use objectid as taskid
+    then insert it into taskinfo collections
+    and the objectid would be returned by insert() function
+    db = conn.tasksinfo
+    db.collection_names()
+    for task in db.tasksinfo.find():print task
+    """
     req = ast.literal_eval(data)
-    #use objectid as taskid
-    #then insert it into taskinfo collections
-    #and the objectid would be returned by insert() function
-    #>>> db = conn.tasksinfo
-    #>>> db.collection_names()
-    #for task in db.tasksinfo.find():print task
     req_id = req['_id']
-    task_id = TASKSINFO.tasksinfo.insert({"req_id": req_id, "vul_num": -1, "resp": "", "start_time":datetime.datetime.utcnow()})
+    task_id = TASKSINFO.tasksinfo.insert(
+        {"req_id": req_id,
+         "vul_num": -1,
+         "resp": "",
+         "start_time": datetime.datetime.utcnow()})
     print req['path'], task_id
     return str(task_id)
+
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -59,8 +88,9 @@ def main():
     while True:
         connection, master_address = sock.accept()
         try:
-            task = recv_timeout(connection)
-            task_id = scan_task(task)
+            task = recv_end(connection, END_SYMBOL)
+            if task[0:1] == "0":
+                task_id = scan_task(task[1:])
             connection.sendall(task_id)
         finally:
             connection.close()
--- scanner/server/master.py
+++ scanner/server/master.py
@@ -9,6 +9,7 @@ parser = SafeConfigParser()
 parser.read("config.ini")
 NUM2SEND = int(parser.get('Master', 'num2send'))
 AGENTPORT = int(parser.get('Agent', 'port'))
+END_SYMBOL = parser.get('Protocol', 'end_symbol')
 
 CONN = pymongo.Connection("localhost", 27017)
 DB = CONN["reqs"]
@@ -18,17 +19,30 @@ def get_agent_ip():
     return "127.0.0.1"
 
 
-def single_scan(tasks):
+def single_scan(req):
+    send_task("0" + str(req) + END_SYMBOL)
+
+
+def send_task(data):
+    """
+    I think protobuf is too heavy for my case
+    Defining an ad hoc protocol works better in this simple case.
+    The format should be:MODE|message|END_SYMBOL
+    MODE=0: Scaning a single http request
+    MODE=1: Scaning all the http requests in the same domain
+    END_SYMBOL: ^@^E!N__D
+    For example: 0{...}^@^E!N__D
+    """
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     agent_ip = get_agent_ip()
     agent_address = (agent_ip, AGENTPORT)
     sock.connect(agent_address)
     try:
-        message = str(tasks)
-        sock.sendall(message)
+        sock.sendall(data)
         taskid = sock.recv(24)
         if len(taskid) != 24:
             gen_log.info("Receive data from agent failed,data: %s", data)
+        sock.close()
         return taskid
     finally:
         sock.close()
@@ -38,14 +52,8 @@ def main():
     docs = DB.requests.find({})
     for doc in docs:
         print doc['path']
+        doc.pop('create_time')
         single_scan(doc)
-#    docs = DB.requests.find({}).limit(NUM2SEND)
-#    for doc in docs:
-#        print doc['path']
-#        single_scan(doc)
-#    while(docs.hasNext()):
-#        more = docs.hasNext()
-# do something with more
 
 if __name__ == "__main__":
     main()
